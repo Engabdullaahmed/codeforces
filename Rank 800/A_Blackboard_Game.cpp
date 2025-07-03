34c7acacda0b5e94dce83ae45776f7d3d27d7542
Engabdullaahmed <ahmedmoustafaabdelwahed@gmail.com>
2025-07-03 19:53:43 +0300

    keep going

---
+++ Rank 800/A_Blackboard_Game.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main()
+{
+    int t;
+    cin >> t ;
+    while (t--)
+    {
+        int m ;
+        cin >> m ;
+       if(m%4==0)
+       {
+        cout<< "BOb"<< endl;
+       }
+       else 
+       {
+        cout<<"Alice" << endl;
+       }
+       
+    }
+    
+}
