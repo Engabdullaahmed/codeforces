3cbeb5e237612c664c9dad9bf7f76b8252a4d657
Engabdullaahmed <ahmedmoustafaabdelwahed@gmail.com>
2025-08-02 20:44:49 +0300

    clockwork nice

---
+++ Rank 900/B_The_Picky_Cat.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    long long tt;
+    cin >> tt;
+    while (tt--)
+    {
+        long long n;
+        cin >> n;
+        long long arr[n + 10], value;
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> arr[i];
+            arr[i] = abs(arr[i]);
+            if (i == 1)
+            {
+                value = arr[i];
+            }
+        }
+        sort(arr + 1, arr + n + 1);
+        int f = 0;
+        for (int i = 1; i <= (n + 1) / 2; i++)
+        {
+            if (arr[i] == value)
+            {
+                f = 1;
+                break;
+            }
+        }
+
+        if (f == 1 || (n % 2 == 0 && arr[n / 2 + 1] == value))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
