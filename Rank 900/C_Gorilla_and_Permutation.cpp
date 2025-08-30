2772676f76da1b78a7f36f959893455d7c43a231
Engabdullaahmed <ahmedmoustafaabdelwahed@gmail.com>
2025-08-30 14:50:13 +0300

    don't give up

---
+++ Rank 900/C_Gorilla_and_Permutation.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*C. Gorilla and Permutation
+time limit per test
+2 seconds
+memory limit per test
+256 megabytes
+
+Gorilla and Noobish_Monk found three numbers n
+, m, and k (m<k). They decided to construct a permutation† of length n
+
+.
+
+For the permutation, Noobish_Monk came up with the following function: g(i)
+is the sum of all the numbers in the permutation on a prefix of length i that are not greater than m. Similarly, Gorilla came up with the function f, where f(i) is the sum of all the numbers in the permutation on a prefix of length i that are not less than k. A prefix of length i is a subarray consisting of the first i
+
+elements of the original array.
+
+For example, if n=5
+, m=2, k=5, and the permutation is [5,3,4,1,2]
+
+, then:
+
+    f(1)=5
+
+, because 5≥5; g(1)=0, because 5>2
+;
+f(2)=5
+, because 3<5; g(2)=0, because 3>2
+;
+f(3)=5
+, because 4<5; g(3)=0, because 4>2
+;
+f(4)=5
+, because 1<5; g(4)=1, because 1≤2
+;
+f(5)=5
+, because 2<5; g(5)=1+2=3, because 2≤2
+
+    . 
+
+Help them find a permutation for which the value of (∑ni=1f(i)−∑ni=1g(i))
+
+is maximized.
+
+†
+A permutation of length n is an array consisting of n distinct integers from 1 to n in any order. For example, [2,3,1,5,4] is a permutation, but [1,2,2] is not a permutation (as 2 appears twice in the array) and [1,3,4] is also not a permutation (as n=3, but 4
+
+appears in the array).
+Input
+
+The first line contains a single integer t
+(1≤t≤104
+
+)  — the number of test cases.
+
+The only line of each case contains three integers n
+, m, k (2≤n≤105; 1≤m<k≤n
+
+) — the size of the permutation to be constructed and two integers.
+
+It is guaranteed that the sum of n
+over all test cases does not exceed 2⋅105
+
+.
+Output
+
+For each test case, output the permutation  — a set of numbers that satisfies the conditions of the problem. If there are multiple solutions, output any of them.
+Example
+Input
+Copy
+
+3
+5 2 5
+3 1 3
+10 3 8
+
+Output
+Copy
+
+5 3 4 1 2
+3 2 1
+10 9 8 4 7 5 6 1 2 3
+
+Note
+
+In the first example, (∑ni=1f(i)−∑ni=1g(i))=5⋅5−(0⋅3+1+3)=25−4=21
+
+*/
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, m, k;
+        cin >> n >> m >> k;
+
+        vector<long long> arr(n + 1);
+        vector<long long> ans(n + 1, 0);
+
+        for (int i = 1; i <= n; i++)
+        {
+            arr[i] = i;
+        }
+
+        for (int i = n; i >= k; i--)
+        {
+            ans[n - i + 1] = arr[i];
+            arr[i] = 0;
+        }
+
+        int x = n;
+        for (int i = m; i > 0; i--)
+        {
+            ans[x] = arr[i];
+            x--;
+            arr[i] = 0;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            if (arr[i] != 0 && ans[i] == 0)
+            {
+                ans[i] = arr[i];
+                arr[i] = 0;
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (arr[i] != 0)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (ans[j] == 0)
+                    {
+                        ans[j] = arr[i];
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
+    }
+}
